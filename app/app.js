c9d3275e6dcce4f1df0231018749d2bff1d38a6b
nadav <nadavoxenberg@gmail.com>
2024-09-16 09:32:24 +0300

    remove spaces

--- app/app.js
+++ app/app.js
@@ -3,16 +3,9 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 
-// For JSON payloads
 app.use(express.json());
-
-// For URL-encoded payloads
 app.use(express.urlencoded({ extended: true }));
-
-// Import routes
 const routes = require('./routes');
-
-// Use the routes
 app.use('/', routes);
 
 app.listen(port, () => {
